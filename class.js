03d79411ff01b160192e1f9617847eed066ebc9d
unknown <siamur01@gmail.com>
2021-08-25 02:44:44 +0600

    n

--- class.js
+++ class.js
@@ -1,27 +1,32 @@
-/*STEP----1/////////
+/*////////////////////////////STEP----1//////////////////
 class Support {
    name;
    designation = 'Support Web Dev';
    startSession() {
       console.log('start a support session');
 
+      ////////////////////////////////////////comment///////////////////////////////////////////////eta(startSession) ekta function(class er vitore function likha zai tobe  function keyword likha zabe na...sorasori function er nam likha lagbe///Suport t aekta class)
+      /////////////comment//////////////////////////////////////////////
    }
 }
 const aamir = new Support();
 console.log(aamir);*/
 
-/*STEP-222/////////////////////////////////
+/*//////////////////////////////STEP-222/////////////////////////////////
 class Support {
    name;
    role = 'Support Web Dev';
    address = 'BD';
    startSession() {
       console.log('start a support session');
-
    }
 }
 const aamir = new Support();
 console.log(aamir);*/
+// object banaite hole object er ekta nam dite hobe......ekhane nam (aamir)/////////////////
+
+
+
 
 /*STEP-3//////////////////
 class Support {
@@ -30,10 +35,11 @@ class Support {
    address = 'BD';
     constructor(name) {
       this.name = name
+ /////////////////class start korar ekta function constractor (special ekta function)//////////
    }
+
    startSession() {
       console.log('start a support session');
-
    }
 }
 const aamir = new Support();
@@ -88,7 +94,6 @@ class Support {
    }
    startSession() {
       console.log(this.name, 'start a support session');
-
    }
 }
 const aamir = new Support('Amir Khan', 'BD');
@@ -100,3 +105,5 @@ aamir.startSession();
 salman.startSession();
 console.log(aamir, salman, sharukh, akshay);
 // console.log()
+
+// class er vitore kon property access korte chaile (this.) diye access kora zai this.name this.address this.id 
--- inheritance.js
+++ inheritance.js
@@ -7,49 +7,67 @@ class TeamMember {
       this.address = address;
    }
 }
-
-
 class Support extends TeamMember {
+   /* name;
+    designation = 'Support Web Dev';
+    address = 'BD';
+    constructor(name, address) {
+       this.name = name;
+       this.address = address;
+    }*/
    groupSupportTime;
    constructor(name, address, time) {
-      super(name, address);
+      super(name, address);//super extension ....mane uporer level er super type er kichu 
       this.groupSupportTime = time;
    }
    startSession() {
-      // console.log(this.name, 'start a support session');
-
+      console.log(this.name, 'start a support session');
    }
 }
-
 class StudentCare extends TeamMember {
-   buildARoutine(student) {
-      // console.log(student.name, 'Build a routine for', student)
-   }
-}
-class NeptuneDev {
-   name;
+   /*name;
    designation = 'Student Care Web Deb';
    address = 'BD';
    constructor(name, address) {
       this.name = name;
       this.address = address;
+   }*/
+   buildARoutine(student) {
+      console.log(this.name, 'Build a routine for', student)
+   }
+}
+class NeptuneDev extends TeamMember {
+   /*   name;
+   designation = 'Student Care Web Deb';
+   address = 'BD';
+   constructor(name, address) {
+      this.name = name;
+      this.address = address;
+   }*/
+   CodeEditor;
+   constructor(name, editor, address) {
+      super(name, address);
+      this.CodeEditor = editor;
    }
    releaseApp(version) {
-      // console.log(student.name, 'release app version', version)
+      console.log(this.name, 'release app version', version)
    }
 }
 const aamir = new Support('Amir Khan', 'BD', 11);
-const salman = new Support('Solaiman khan', 'Dubai', 20);
+const salman = new Support('Solaiman khan', 'Dubai', 4);
 const sharukh = new Support('Shahrukh khan', 'Dubai');
-const akshay = new Support('Akshay kumar', 'Dubai', 3);
+const akshay = new Support('Akshay kumar', 'Dubai', 9);
 
-const alia = new StudentCare('Alia Bhatt', 'Mumbai');
+const alia = new StudentCare('Alia Bhatt', 'Mumbai', 3);
+const ash = new NeptuneDev('Assh riya', 'chennaii', 'android studio');
+ash.releaseApp('1.2.5');
 
 // aamir.startSession();
 // salman.startSession();
 // console.log(aamir, salman, sharukh, akshay);
-// console.log(aamir);
-// console.log(alia);
+console.log(aamir);
+console.log(alia);
+// console.log(ash);
 
 
 /*step---1//////////////////.................../////////
